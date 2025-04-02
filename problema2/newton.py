2efd8a6a8eccd10c5d1bfce1d9b5b366e24aff01
Verônica Martins <145865547+VegaMartins@users.noreply.github.com>
2025-04-02 19:10:58 -0300

    adição de comentarios

--- problema2/newton.py
+++ problema2/newton.py
@@ -1,23 +1,23 @@
 import math
 
 def f(x):
-    return math.cos(x) - x  # Função do problema cos(x) - x
+    return math.cos(x) - x  # função do problema cos(x) - x
 
 def df(x):
-    return -math.sin(x) - 1  # Derivada da função: -sin(x) - 1
+    return -math.sin(x) - 1  # derivada da função: -sin(x) - 1
 
 def metodo_newton(f, df, x, tol1=1e-4, tol2=1e-4, max_iter=25): # método de Newton-Raphson
-    if abs(f(x)) < tol1:
+    if abs(f(x)) < tol1: # verificação se o chute inicial já é uma raiz 
         return x, 0, 0, f(x)
     
     k = 1
     diferenca = float('inf')
     while True:
-        x1 = x - f(x) / df(x)  # Ajusta a estimativa na direção da raiz usando a tangente
-        diferenca = abs(x1 - x) # Diferença entre as estimativas 
+        x1 = x - f(x) / df(x)  # formula de Newton-Raphson, justa a estimativa na direção da raiz usando a tangente
+        diferenca = abs(x1 - x) # diferença entre as estimativas 
         print(f"Iteração {k}: x = {x1:.8f}, f(x) = {f(x1):+.2e}")
         
-        if abs(f(x1)) < tol1 or diferenca < tol2 or k >= max_iter: # Verificação das condições de parada
+        if abs(f(x1)) < tol1 or diferenca < tol2 or k >= max_iter: # verificação das condições de parada
             return x1, k, diferenca, f(x1)
         
         x = x1
